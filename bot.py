9e28e672f3945a353ffbef3d2de487ded95d3f3f
Kamil Kierski <cybernetic87@gmail.com>
2020-09-12 19:45:06 +0200

    initial commit

---
+++ bot.py
@@ -0,0 +1,48 @@
+import ch
+from gtts import gTTS
+# from mpyg321.mpyg321 import MPyg321Player
+
+import os
+
+class bot(ch.RoomManager):
+    def onInit(self):
+        self.setNameColor("FFFFFF")
+        self.setFontColor("FFFFFF")
+        self.setFontFace("Ariel")
+        self.setFontSize(11)
+        # player = MPyg321Player()
+
+    def onMessage(self, room, user, message):
+        print("[{0}] {1}: {2}".format(room.name, user.name.title(), message.body))
+        self.read_message(user.name.title(), message.body)
+        try:
+            cmd, args = message.body.split(" ", 1)
+        except:
+            cmd, args = message.body, ""
+
+        if cmd[0] == "!":
+            prfx = True
+            cmd = cmd[1:]
+        else:
+            prfx = False
+
+        if cmd.lower() == "say" and prfx:
+            room.message(args)
+
+    def read_message(self, user, message):
+        language = 'pl'
+
+        text = user + ' ' + message
+        print(f'Reading {text}')
+        myobj = gTTS(text=text, lang=language, slow=False)
+
+        myobj.save("message.mp3")
+        # self.player.play_song("message.mp3")
+        os.system("mpg123 message.mp3")
+
+
+rooms = ['strimiworld']
+username = "czytamstrimsy"
+password = "freeflet"
+
+bot.easy_start(rooms, username, password)
