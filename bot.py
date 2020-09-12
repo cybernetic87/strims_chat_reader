e636404bdb0be73a2a133508a78c81e5755cb5ca
Kamil Kierski <cybernetic87@gmail.com>
2020-09-12 20:20:50 +0200

    removed cmd - performance improvement

--- bot.py
+++ bot.py
@@ -15,19 +15,6 @@ class bot(ch.RoomManager):
     def onMessage(self, room, user, message):
         print("[{0}] {1}: {2}".format(room.name, user.name.title(), message.body))
         self.read_message(user.name.title(), message.body)
-        try:
-            cmd, args = message.body.split(" ", 1)
-        except:
-            cmd, args = message.body, ""
-
-        if cmd[0] == "!":
-            prfx = True
-            cmd = cmd[1:]
-        else:
-            prfx = False
-
-        if cmd.lower() == "say" and prfx:
-            room.message(args)
 
     def read_message(self, user, message):
         language = 'pl'
