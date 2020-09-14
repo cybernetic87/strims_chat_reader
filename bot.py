0c7c15340513c327ccfd85c7d0fc43d8061ef6eb
Kamil Kierski <cybernetic87@gmail.com>
2020-09-14 21:51:05 +0200

    fixes

--- bot.py
+++ bot.py
@@ -18,29 +18,20 @@ class bot(ch.RoomManager):
         print("[{0}] {1}: {2}".format(room.name, user.name.title(), message.body))
         self.read_message(user.name.title(), message.body)
 
-    # def read_message(self, user, message):
-    #     language = 'pl'
-    #
-    #     text = user + ' ' + message
-    #     print(f'Reading {text}')
-    #     myobj = gTTS(text=text, lang=language, slow=False)
-    #
-    #     myobj.save("message.mp3")
-    #     # self.player.play_song("message.mp3")
-    #     os.system("mpg123 message.mp3")
-
 
     def read_message(self, user, message):
-        with io.BytesIO() as f:
-            language = 'pl'
-            text = user + ' ' + message
-            gTTS(text=text, lang=language).write_to_fp(f)
-            f.seek(0)
-            pygame.mixer.init()
-            pygame.mixer.music.load(f)
-            pygame.mixer.music.play()
-            while pygame.mixer.music.get_busy():
-                continue
+        if len(message) < 100:
+            with io.BytesIO() as f:
+                language = 'pl'
+                text = user + ' ' + message
+                text = ''.join([i for i in text if not i.isdigit()])
+                gTTS(text=text, lang=language).write_to_fp(f)
+                f.seek(0)
+                pygame.mixer.init()
+                pygame.mixer.music.load(f)
+                pygame.mixer.music.play()
+                while pygame.mixer.music.get_busy():
+                    continue
 
 
 rooms = ['strimiworld']
