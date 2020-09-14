a29690a014ba21bd23e0725b15525d45375ffd5f
Kamil Kierski <cybernetic87@gmail.com>
2020-09-14 21:11:08 +0200

    test

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import ch
+import io
+import pygame
 from gtts import gTTS
-# from mpyg321.mpyg321 import MPyg321Player
+
 
 import os
 
@@ -16,16 +18,29 @@ class bot(ch.RoomManager):
         print("[{0}] {1}: {2}".format(room.name, user.name.title(), message.body))
         self.read_message(user.name.title(), message.body)
 
+    # def read_message(self, user, message):
+    #     language = 'pl'
+    #
+    #     text = user + ' ' + message
+    #     print(f'Reading {text}')
+    #     myobj = gTTS(text=text, lang=language, slow=False)
+    #
+    #     myobj.save("message.mp3")
+    #     # self.player.play_song("message.mp3")
+    #     os.system("mpg123 message.mp3")
+
+
     def read_message(self, user, message):
-        language = 'pl'
-
-        text = user + ' ' + message
-        print(f'Reading {text}')
-        myobj = gTTS(text=text, lang=language, slow=False)
-
-        myobj.save("message.mp3")
-        # self.player.play_song("message.mp3")
-        os.system("mpg123 message.mp3")
+        with io.BytesIO() as f:
+            language = 'pl'
+            text = user + ' ' + message
+            gTTS(text=text, lang=language).write_to_fp(f)
+            f.seek(0)
+            pygame.mixer.init()
+            pygame.mixer.music.load(f)
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                continue
 
 
 rooms = ['strimiworld']
